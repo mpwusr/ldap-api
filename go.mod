f4d54dfebe84ddf8d83ad0067b9b5d27998f1f16
mpwusr <mpw10128@gmail.com>
2022-11-28 21:56:29 -0500

    first commit of api-example

---
+++ go.mod
@@ -0,0 +1,8 @@
+module ldap-api
+
+go 1.19
+
+require (
+	github.com/go-ldap/ldap v3.0.3+incompatible // indirect
+	gopkg.in/asn1-ber.v1 v1.0.0-20181015200546-f715ec2f112d // indirect
+)
---
+++ main.go
@@ -0,0 +1,122 @@
+package ldap_api
+
+import (
+	"fmt"
+	"github.com/go-ldap/ldap"
+	"log"
+)
+
+const (
+	BindUsername = "admin@example.com"
+	BindPassword = "Topdog88"
+	FQDN         = "ldap.example.com"
+	BaseDN       = "cn=ldap,dc=example,dc=com"
+	Filter       = "(objectClass=*)"
+)
+
+// Ldap Connection without TLS
+func Connect() (*ldap.Conn, error) {
+	// You can also use IP instead of FQDN
+	l, err := ldap.DialURL(fmt.Sprintf("ldap://%s:389", FQDN))
+	if err != nil {
+		return nil, err
+	}
+
+	return l, nil
+}
+
+// Ldap Connection with TLS
+func ConnectTLS() (*ldap.Conn, error) {
+	// You can also use IP instead of FQDN
+	l, err := ldap.DialURL(fmt.Sprintf("ldaps://%s:636", FQDN))
+	if err != nil {
+		return nil, err
+	}
+
+	return l, nil
+}
+
+// Anonymous Bind and Search
+func AnonymousBindAndSearch(l *ldap.Conn) (*ldap.SearchResult, error) {
+	l.UnauthenticatedBind("")
+
+	anonReq := ldap.NewSearchRequest(
+		"",
+		ldap.ScopeBaseObject, // you can also use ldap.ScopeWholeSubtree
+		ldap.NeverDerefAliases,
+		0,
+		0,
+		false,
+		Filter,
+		[]string{},
+		nil,
+	)
+	result, err := l.Search(anonReq)
+	if err != nil {
+		return nil, fmt.Errorf("Anonymous Bind Search Error: %s", err)
+	}
+
+	if len(result.Entries) > 0 {
+		result.Entries[0].Print()
+		return result, nil
+	} else {
+		return nil, fmt.Errorf("Couldn't fetch anonymous bind search entries")
+	}
+}
+
+// Normal Bind and Search
+func BindAndSearch(l *ldap.Conn) (*ldap.SearchResult, error) {
+	l.Bind(BindUsername, BindPassword)
+
+	searchReq := ldap.NewSearchRequest(
+		BaseDN,
+		ldap.ScopeBaseObject, // you can also use ldap.ScopeWholeSubtree
+		ldap.NeverDerefAliases,
+		0,
+		0,
+		false,
+		Filter,
+		[]string{},
+		nil,
+	)
+	result, err := l.Search(searchReq)
+	if err != nil {
+		return nil, fmt.Errorf("Search Error: %s", err)
+	}
+
+	if len(result.Entries) > 0 {
+		return result, nil
+	} else {
+		return nil, fmt.Errorf("Couldn't fetch search entries")
+	}
+}
+
+func main() {
+	// Non-TLS Connection
+	l, err := Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+
+	// Anonymous Bind and Search
+	result, err := AnonymousBindAndSearch(l)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result.Entries[0].Print()
+	
+	// TLS Connection
+	l, err := ConnectTLS()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+
+	// Normal Bind and Search
+	result, err = BindAndSearch(l)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result.Entries[0].Print()
+}
